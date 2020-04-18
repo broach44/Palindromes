957a4b1fa078fae5b2e45b9e1e4c0e5eef276186
Crystal Broach <crysland@gmail.com>
2020-04-18 13:30:22 -0500

    Tests written and method correct

--- Palindromes.Tests/word_is_palindrome.cs
+++ Palindromes.Tests/word_is_palindrome.cs
@@ -3,24 +3,83 @@ using Xunit;
 
 namespace Palindromes.Tests
 {
-    public class word_is_palindrome
+    public class Word_is_palindrome
     {
         [Fact]
-        public void Test1()
+        public void Check_If_Stars_Is_Palindrome()
         {
             //Stars = false
+            //Arrange
+            var word = "Stars";
+            var wordFlipper = new Flipper();
 
+            //Act
+            var actualResult = wordFlipper.WordFlip(word);
+            //Assert
+            Assert.False(actualResult);
+        }
 
+        [Fact]
+        public void Check_If_OaKakUwakovLil()
+        {
             //O, a kak Uwakov lil vo kawu kakao! = true
+            //Arrange
+            var word = "O, a kak Uwakov lil vo kawu kakao!";
+            var wordFlipper = new Flipper();
 
+            //Act
+            var actualResult = wordFlipper.WordFlip(word);
 
+            //Assert
+            Assert.True(actualResult);
+
+        }
+
+        [Fact]
+        public void Check_If_SomeMenInterpret()
+        {
             //Some men interpret nine memos = true
+            //Arrange
+            var word = "Some men interpret nine memos";
+            var wordFlipper = new Flipper();
 
+            //Act
+            var actualResult = wordFlipper.WordFlip(word);
 
+            //Assert
+            Assert.True(actualResult);
+        }
+
+        [Fact]
+        public void Check_If_Tacocat()
+        {
             //tacocat = true
+            //Arrange
+            var word = "Tacocat";
+            var wordFlipper = new Flipper();
 
+            //Act
+            var actualResult = wordFlipper.WordFlip(word);
 
+            //Assert
+            Assert.True(actualResult);
+        }
+
+        [Fact]
+        public void Check_If_Crystal()
+        {
             //crystal = false
+            //Arrange
+            var word = "Crystal";
+            var wordFlipper = new Flipper();
+
+            //Act
+            var actualResult = wordFlipper.WordFlip(word);
+
+            //Assert
+            Assert.False(actualResult);
         }
     }
+
+
 }
---
+++ Palindromes/Flipper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindromes
+{
+    public class Flipper
+    {
+        public bool WordFlip(string someWord)
+        {
+
+            var wordToTest = someWord.ToLower().Replace(" ", "").Replace("!", "").Replace(",", "");
+            var wordCharArray = wordToTest.ToCharArray();
+            var wordStringArray = Array.ConvertAll(wordCharArray, char.ToString);
+            Array.Reverse(wordStringArray);
+            var reversedWord = string.Join("", wordStringArray);
+
+            if (reversedWord == wordToTest)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
