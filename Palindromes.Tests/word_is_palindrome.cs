85f1d4cad51e5ec09d8a5d24a626dd06e3ed1ca1
Crystal Broach <crysland@gmail.com>
2020-04-17 20:37:56 -0500

    Add project files.

---
+++ Palindromes.Tests/word_is_palindrome.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Palindromes.Tests
+{
+    public class word_is_palindrome
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Stars = false
+
+
+            //O, a kak Uwakov lil vo kawu kakao! = true
+
+
+            //Some men interpret nine memos = true
+
+
+            //tacocat = true
+
+
+            //crystal = false
+        }
+    }
+}
